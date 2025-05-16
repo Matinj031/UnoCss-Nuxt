bc03a81739c7d4f7e8649a6e52a50d1749d46d91
Matin Jahi <Matin13822332@gmail.com>
2025-05-16 20:25:02 +0330

    chore: add Unocss presets and Iconify JSON Tabler for enhanced styling

--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,17 @@
+import presetMini from '@unocss/preset-mini'
+import presetAttributify from '@unocss/preset-attributify'
+import presetIcons from '@unocss/preset-icons'
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
+    presets:[
+        presetMini(),
+        presetAttributify(),
+        presetIcons()
+    ],
+    shortcuts: {
+        // shortcuts to multiple utilities
+        'btn': 'py-2 px-4 font-semibold rounded-lg shadow-md',
+        'btn-green': 'text-white bg-green-500 hover:bg-green-700',
+    }
 })
